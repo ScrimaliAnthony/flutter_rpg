301426bbed82ae73e080e02a62e4075516c7de9a
Scrimali Anthony <anthony.scrimali@gmail.com>
2024-03-17 21:48:14 +0100

    Ajout du Widget Card

--- lib/theme.dart
+++ lib/theme.dart
@@ -41,4 +41,12 @@ ThemeData primaryTheme = ThemeData(
         fontWeight: FontWeight.bold,
         letterSpacing: 2,
       )),
+
+  // Card theme
+  cardTheme: CardTheme(
+      color: AppColors.secondaryColor.withOpacity(0.5),
+      surfaceTintColor: Colors.transparent,
+      shape: const RoundedRectangleBorder(),
+      shadowColor: Colors.transparent,
+      margin: const EdgeInsets.only(bottom: 16)),
 );
